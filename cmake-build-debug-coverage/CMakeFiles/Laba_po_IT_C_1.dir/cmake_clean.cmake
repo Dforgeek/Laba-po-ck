4e26fde9dd20e0f4e41b24d9f4ca111edcced1b7
Dforgeek <1812stepanovsky@gmail.com>
2021-09-24 02:35:25 +0300

    Сделал работу с действительными числами, перевёл программу на англ

---
+++ cmake-build-debug-coverage/CMakeFiles/Laba_po_IT_C_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba_po_IT_C_1.dir/main.cpp.obj"
+  "Laba_po_IT_C_1.exe"
+  "Laba_po_IT_C_1.exe.manifest"
+  "Laba_po_IT_C_1.pdb"
+  "libLaba_po_IT_C_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laba_po_IT_C_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
