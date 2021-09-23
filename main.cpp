af5f628b71177a6f6b77a5bba208fe5d936a37ef
Dforgeek <1812stepanovsky@gmail.com>
2021-09-23 20:25:58 +0300

    Сделал работу с действительными числами, перевёл программу на англ

--- main.cpp
+++ main.cpp
@@ -9,18 +9,19 @@ int main() {
     cout << ("Enter 2 integer numbers\n>");
     cin >> a >> b;
     cout << ("What do you want to do with this numbers?\n");
-    cout << ("Enter \"1\", \"2\", \"3\", \"4\" for addition, subtraction, multiplication or dividing \n>");
+    cout << ("Enter \"1\", \"2\", \"3\", \"4\" for addition, subtraction, multiplication or dividing \n"
+             "(if you will try to divide by zero, you'll get \"inf\")\n>");
     cin >> input_com;
     if (input_com == 1) {
         cout << "And the result is:" << (a + b);
     } else if (input_com == 2) {
         cout << "And the result is:" << (a - b);
     } else if (input_com == 3) {
-        cout << setprecision(3) << fixed;
+        cout << setprecision(7) << fixed;
         cout << "And the result is:" << (a * b);
     } else if (input_com == 4) {
-        cout << setprecision(3) << fixed;
-        cout << "And the result is:"  << (a / b);
+        cout << setprecision(7) << fixed;
+        cout << "And the result is:" << (a / b);
     }
     return 0;
 }
